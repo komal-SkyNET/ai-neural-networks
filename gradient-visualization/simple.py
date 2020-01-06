27a5d50395bc7e518dc4f794f7c391ac063ab3c3
komal-SkyNET <komal.venkateshg@gmail.com>
2020-01-06 17:10:24 +1100

    Add a neural network to understand gradient descent & derivative

---
+++ gradient-visualization/simple.py
@@ -0,0 +1,77 @@
+import numpy as np
+import random as r
+import sys
+
+def sigmoid(ip, derivate=False):
+    if derivate:
+        return ip*(1-ip)
+    return 1.0/(1+np.exp(-1*ip))
+
+class NeuralNet:
+    global sigmoid 
+
+    def __init__(self):
+        self.inputLayers = 2
+        self.outputLayer = 1
+        self.bias = r.random()
+    
+    def setup(self):
+        self.i = np.array([r.random(), r.random()], dtype=float).reshape(2,)
+        self.w = np.array([r.random(), r.random()], dtype=float).reshape(2,)
+
+    def forward_propogate(self):
+        self.z = self.w*self.i
+        self.o = sigmoid(sum(self.z)-self.bias)
+    
+    def optimize_cost(self, desired):
+        i=0
+        current_cost = pow(desired - self.o, 2)
+        for weight in self.w:
+            dpdw =  -1*(desired-self.o) * (sigmoid(self.o, derivate=True)) * self.i[i]
+            self.w[i] = self.w[i] - 2*dpdw
+            i+=1
+        #calculate dp/dB
+        dpdB = -1*(desired-self.o) * (sigmoid(self.o, derivate=True)) * -1
+        self.bias = self.bias - 2*dpdB
+        self.forward_propogate()
+    
+    def train(self, ip, op):
+        self.i = np.array(ip).reshape(2,)
+        self.forward_propogate()
+        self.optimize_cost(op[0])
+
+n = NeuralNet()
+n.setup()
+# while sys.stdin.read(1):
+success_rate = 0
+trial=0
+done = False
+while not done:
+    a = [0.1,1,0.1,1]
+    b = [0.1,0.1,1,1]
+    c = [0,0,0,1]
+    for i in range(len(a)):
+        trial +=1
+        n.train([a[i],b[i]],[c[i]])
+        if c[i] - n.o < 0.01:
+            success_rate +=1
+            print(100*success_rate/trial, "%")
+        if 100*success_rate/trial > 99 and trial > 4:
+            print(100*success_rate/trial, "%")
+            print("Network trained, took: {} trials".format(trial))
+            print("Network weights:{}, bias:{}".format(n.w, n.bias))
+            done = True
+            break
+
+while True:
+    print("-----")
+    print("Enter ip1 & 2")
+    ip1 = float(sys.stdin.readline())
+    ip2 = float(sys.stdin.readline())
+    print(ip1, ip2)
+    n.i = np.array([ip1, ip2]).reshape(2,)
+    n.forward_propogate()
+    print("network output:{}".format(n.o))
+    
+
+    
