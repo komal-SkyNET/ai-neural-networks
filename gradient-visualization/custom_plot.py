149561b3d872521fcb43f366f52f565d4d05590c
komal-SkyNET <komal.venkateshg@gmail.com>
2020-01-16 17:49:19 +1100

    Regression surface analysis plt & anim thread

--- gradient-visualization/custom_plot.py
+++ gradient-visualization/custom_plot.py
@@ -1,31 +1,43 @@
+import os
 import matplotlib.pyplot as plt
 from mpl_toolkits import mplot3d
-from matplotlib.animation import FuncAnimation
+from matplotlib.animation import FuncAnimation, FFMpegFileWriter
 import numpy as np 
-import os
+import threading
 dir_path = os.path.dirname(os.path.realpath(__file__))
 _window = 0
 _fig = plt.figure()
+
 def sigmoid(ip, derivate=False):
     if derivate:
         return ip*(1-ip)
     return 1.0/(1+np.exp(-1*ip))
 
-class Plot:
+def plt_show():
+    '''Text-blocking version of plt.show()
+    Use this instead of plt.show()'''
+    plt.draw()
+    plt.pause(0.01)
+    # plt.close()
+
+class Plot(threading.Thread):
 
     def __init__(self):
         global _window, _fig
         self.W1_points = []
         self.W2_points = []
+        self.bias_points = []
         self.Z_points = []
         self.fig = _fig
+        self.show_animation = True
+        self.lock = threading.Lock()
         _window+=1
     
     def draw_3d_lineplot(self, label='LinePlot', x_label='x', y_label='y', z_label='z'):
         X = self.W1_points
         Y = self.W2_points
         Z = self.Z_points
-        ax = self.fig.add_subplot(221, projection='3d')
+        ax = self.fig.add_subplot(211, projection='3d')
         ax.plot(X, Y, Z, label=label)
         ax.set_xlabel(x_label)
         ax.set_ylabel(y_label)
@@ -37,28 +49,77 @@ class Plot:
         X = self.W1_points
         Y = self.W2_points
         Z = self.Z_points
-        ax = self.fig.add_subplot(222, projection='3d')
+        ax = self.fig.add_subplot(212, projection='3d')
         ax.scatter(X, Y, Z, label=label)
         ax.set_xlabel(x_label)
         ax.set_ylabel(y_label)
         ax.set_zlabel(z_label)
         ax.view_init(30, 250)
     
-    def draw_3d_regression_analysis_surface(self,  bias):
+    def draw_3d_regression_analysis_surface(self):
         global sigmoid
-        title = "Z = sigmoid({}x+ {}y - {})".format(round(self.W1_points[-1],5), round(self.W2_points[-1],5), round(bias,5))
-        ax = self.fig.add_subplot(223, projection='3d')
+        title = "Z = sigmoid({}x+ {}y - {})".format(round(self.W1_points[-1],5), round(self.W2_points[-1],5), round(self.bias_points[-1],5))
+        ax = self.fig.add_subplot(111, projection='3d')
+        ax.view_init(30, 250)
+        ax.set_title(title)
         x = np.linspace(0,1,50)
         y = np.linspace(0,1,50)
         X, Y = np.meshgrid(x,y)
-        Z = sigmoid(self.W1_points[-1]*X + self.W2_points[-1]*Y - bias)
-        ax.view_init(30, 250)
-        ax.set_title(title)
+        Z = sigmoid(self.W1_points[-1]*X + self.W2_points[-1]*Y - self.bias_points[-1])
         ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
                         cmap='viridis', edgecolor='none')
         ax.contour3D(X, Y, Z, 50, cmap='binary')
 
-    def draw_surface(self,ax, f_z=None):
+    def init_animate_regression(self):
+        global sigmoid
+        x = np.linspace(0,1,20)
+        y = np.linspace(0,1,20)
+        X, Y = np.meshgrid(x,y)
+        with self.lock:
+            title = "Z = sigmoid({}x+ {}y - {})".format(round(self.W1_points[-1],5), round(self.W2_points[-1],5), round(self.bias_points[-1],5))
+            Z = sigmoid(self.W1_points[0]*X + self.W2_points[0]*Y - self.bias_points[0])
+        ax = self.fig.add_subplot(111, projection='3d')
+        ax.view_init(30, 250)
+        ax.set_title(title)
+        surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                        cmap='viridis', edgecolor='none', label=title)
+        ax.contour3D(X, Y, Z, 50, cmap='binary')
+        self.anim_ax = ax
+        surf._facecolors2d=surf._facecolors3d
+        surf._edgecolors2d=surf._edgecolors3d
+        return surf,
+
+    def update_anim(self, frames):
+        global sigmoid
+        x = np.linspace(0,1,20)
+        y = np.linspace(0,1,20)
+        X, Y = np.meshgrid(x,y)
+        with self.lock:
+            title = "Z = sigmoid({}x+ {}y - {})".format(round(self.W1_points[-1],5), round(self.W2_points[-1],5), round(self.bias_points[-1],5))
+            Z = sigmoid(self.W1_points[-1]*X + self.W2_points[-1]*Y - self.bias_points[-1])
+        print(title)
+        ax = self.anim_ax
+        ax.clear()
+        ax.set_title(title)
+        ax.view_init(30, 250)
+        surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1,
+                        cmap='viridis', edgecolor='none', label=title)
+        ax.contour3D(X, Y, Z, 50, cmap='binary')
+        surf._facecolors2d=surf._facecolors3d
+        surf._edgecolors2d=surf._edgecolors3d
+        if self.show_animation:
+            plt_show()
+        return surf,
+
+    def animate(self):
+        self.anim = FuncAnimation(self.fig, self.update_anim, init_func=self.init_animate_regression, frames=350, interval=1, blit=False)
+
+    def save_anim(self):
+        writer = FFMpegFileWriter()
+        # self.anim.save('a.mp4', writer=writer)
+        self.anim.save('regression_analysis_surface.gif', writer='imagemagick', fps=60)
+
+    def draw_surface(self, ax, f_z=None):
         global _window
         x = np.linspace(-10,10,500)
         y = np.linspace(-10,10,500)
@@ -72,5 +133,3 @@ class Plot:
                         cmap='viridis', edgecolor='none')
         ax.contour3D(X, Y, Z, 50, cmap='binary')
 
-# anim = FuncAnimation(fig, animate, init_func=init,
-#                                frames=200, interval=20, blit=True)
--- gradient-visualization/simple.py
+++ gradient-visualization/simple.py
@@ -2,29 +2,36 @@ from custom_plot import Plot, plt
 import numpy as np
 import random as r
 import sys
-import copy
+import copy, time, threading
 
 def sigmoid(ip, derivate=False):
     if derivate:
         return ip*(1-ip)
     return 1.0/(1+np.exp(-1*ip))
 
+def plt_show():
+    '''Text-blocking version of plt.show()
+    Use this instead of plt.show()'''
+    plt.draw()
+    plt.pause(0.01)
+    # plt.close()
+
 class NeuralNet:
     global sigmoid 
 
-    def __init__(self, disable_bias=False, bias=10):
+    def __init__(self):
         self.inputLayers = 2
         self.outputLayer = 1
+        self.plot1 = Plot()
+    
+    def setup(self, disable_bias=False, bias=10):
+        self.i = np.array([r.random(), r.random()], dtype=float).reshape(2,)
+        self.w = np.array([r.random(), r.random()], dtype=float).reshape(2,)
         if not disable_bias:
             self.bias = r.random()
         else:
             self.bias = bias
-        self.plot1 = Plot()
         self.disable_bias = disable_bias
-    
-    def setup(self):
-        self.i = np.array([r.random(), r.random()], dtype=float).reshape(2,)
-        self.w = np.array([r.random(), r.random()], dtype=float).reshape(2,)
 
     def forward_propogate(self):
         self.z = self.w*self.i
@@ -43,59 +50,83 @@ class NeuralNet:
             dpdB = -1*(desired-self.o) * (sigmoid(self.o, derivate=True)) * -1
             self.bias = self.bias - 2*dpdB
         self.forward_propogate()
-        self.plot1.Z_points.append(current_cost)
-        self.plot1.W1_points.append(self.w[0])
-        self.plot1.W2_points.append(self.w[1])
+        with self.plot1.lock:
+            self.plot1.Z_points.append(current_cost)
+            self.plot1.W1_points.append(self.w[0])
+            self.plot1.W2_points.append(self.w[1])
+            self.plot1.bias_points.append(self.bias)
     
     def train(self, ip, op):
         self.i = np.array(ip).reshape(2,)
         self.forward_propogate()
         self.optimize_cost(op[0])
 
+class TrainThread(threading.Thread):
+
+    def __init__(self, neural_net):
+        super(TrainThread, self).__init__()
+        self.neural_net = neural_net
+        self.neural_net.setup()
+
+    def run(self):
+        success_rate = 0
+        trial=0
+        done = False
+        n = self.neural_net
+        while not done:
+            a = [0.1,1,0.1,1]
+            b = [0.1,0.1,1,1]
+            c = [0,0,0,1]
+            time.sleep(0.01)
+            for i in range(len(a)):
+                trial +=1
+                n.train([a[i],b[i]],[c[i]])
+                if c[i] - n.o < 0.1:
+                    success_rate +=1
+                    print(100*success_rate/trial, "%")
+                if 100*success_rate/trial > 99 and trial > 4:
+                    print(100*success_rate/trial, "%")
+                    print("Network trained, took: {} trials".format(trial))
+                    print("Network weights:{}, bias:{}".format(n.w, n.bias))
+                    done = True
+                    break
+
+class PlotAnimationThread(threading.Thread):
+
+    def __init__(self, neural_net):
+        super(PlotAnimationThread, self).__init__()
+        self.neural_net = neural_net 
+    
+    def animate(self):
+        self.neural_net.plot1.animate()
+    
+    def save_anim(self):
+        self.neural_net.plot1.save_anim()
+    
+    def run(self):
+        self.animate()
+        self.save_anim()
+
+
 n = NeuralNet()
-n.setup()
-# while sys.stdin.read(1):
-success_rate = 0
-trial=0
-done = False
-while not done:
-    a = [0.1,1,0.1,1]
-    b = [0.1,0.1,1,1]
-    c = [0,0,0,1]
-    for i in range(len(a)):
-        trial +=1
-        n.train([a[i],b[i]],[c[i]])
-        if c[i] - n.o < 0.01:
-            success_rate +=1
-            print(100*success_rate/trial, "%")
-        if 100*success_rate/trial > 99 and trial > 4:
-            print(100*success_rate/trial, "%")
-            print("Network trained, took: {} trials".format(trial))
-            print("Network weights:{}, bias:{}".format(n.w, n.bias))
-            done = True
-            break
-n.plot1.draw_3d_lineplot(x_label='weight1', y_label='weight2', z_label='error')
-plot2 = Plot()
-plot2.W1_points = n.plot1.W1_points
-plot2.W2_points = n.plot1.W2_points
-plot2.Z_points = n.plot1.Z_points
-plot2.draw_3d_scatter_plot(x_label='weight1', y_label='weight2', z_label='error')
+# Because matplotlib doesn't like background threads updating GUI WINDOWS
+# if we want to view the updating plot, do not run PlotAnimationThread functions as thread 
+n.plot1.show_animation = False
+trainThread = TrainThread(n)
+trainThread.start()
+time.sleep(0.1)   
+ani_thread = PlotAnimationThread(n)
+ani_thread.daemon = True
+ani_thread.start()
+time.sleep(0.1)
+trainThread.join()
+print("Training thread finished")
+ani_thread.join()
+print('Animation thread finished')
 
-##draw regression line
-plot2.draw_3d_regression_analysis_surface(n.bias)
-
-plt.tight_layout()
-plt.show()
-
-# while True:
-#     print("-----")
-#     print("Enter ip1 & 2")
-#     ip1 = float(sys.stdin.readline())
-#     ip2 = float(sys.stdin.readline())
-#     print(ip1, ip2)
-#     n.i = np.array([ip1, ip2]).reshape(2,)
-#     n.forward_propogate()
-#     print("network output:{}".format(n.o))
-
-#For 0.01 error rate | Network weights:[14.04340991 14.04341878], bias:21.860932213216703
-
+### TODO: Make these plots work alongwith animation plot
+# n.plot1.anim_ax.clear()
+# plt.clf()
+# n.plot1.draw_3d_lineplot()
+# n.plot1.draw_3d_scatter_plot()
+# plt.show()
